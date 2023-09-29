13435fdd60f41893f1222e075cda99d6f7732f26
Rida Rhnizar <rrhnizar@c1r2p6.1337.ma>
2023-09-29 23:31:18 +0100

    first check the parsing in ex00

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -6,20 +6,85 @@
 /*   By: rrhnizar <rrhnizar@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 23:07:06 by rrhnizar          #+#    #+#             */
-/*   Updated: 2023/09/27 23:52:19 by rrhnizar         ###   ########.fr       */
+/*   Updated: 2023/09/29 23:30:52 by rrhnizar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
-int	CheckLine(std::string line)
+// int	FirstCheckLine(std::string line)
+// {
+
+// 	for(size_t i=0; i<line.size(); i++)
+	
+// }
+
+std::string	fillString(std::string line)
 {
-	(void)line;
+	std::string str;
+	for(size_t i=0; i<line.length(); i++)
+	{
+		if (line[i] == '-' || line[i] == ',')
+			break;
+		str += line[i];
+	}
+	return str;
+}
+
+int	CheckLine(std::string line, int check)
+{
+	if (check == 0 && line != "date,exchange_rate")
+		return 1;
+	if(line != "date,exchange_rate")
+	{
+		check = 0;
+		std::string year;
+		std::string month;
+		std::string day;
+		std::string exchange_rate;
+		// if(line[0] == ',' || line[0] == '-' || line[line.length() - 1] == ',' || line[line.length() - 1] == '-')
+		// 	return 1;
+		// for(size_t i=0; i<line.length(); i++)
+		// {
+		// 	if (isdigit(line[i]) == 0 && line[i] != '-' && line[i] != ',')
+		// 		return 1;
+		// }
+		for(size_t i=0; i<line.length(); i++)
+		{
+			if (line[i] == '-')
+			{
+				check++;
+				i++;
+			}
+			if (check == 0)
+				year += line[i];
+			else if (check == 1)
+				month += line[i];
+			else if (check == 2)
+			{
+				if(line[i] == ',')
+				{
+					check = -10;
+					i++;
+				}
+				else
+					day += line[i];
+			}
+			if(check == -10)
+				exchange_rate += line[i];
+		}
+		std::cout << "===>  year  : " << year << std::endl;
+		std::cout << "===>  month : " << month << std::endl;
+		std::cout << "===>  day   : " << day << std::endl;
+		std::cout << "===>  exchange_rate   : " << exchange_rate << std::endl;
+		exit(1);
+	}
 	return 0;
 }
 
 int	fillContainerWithData()
 {
+	int check = 0;
 	std::ifstream fileData;
 	fileData.open("data.csv");
 	std::vector<std::string> container;
@@ -31,9 +96,10 @@ int	fillContainerWithData()
 	std::string line;
 	while(std::getline(fileData, line))
 	{
-		if (CheckLine(line) == 1)
+		if (CheckLine(line, check) == 1)
 			return 1;
 		container.push_back(line);
+		check++;
 	}
 	return 0;
 }
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rrhnizar <rrhnizar@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 23:06:12 by rrhnizar          #+#    #+#             */
-/*   Updated: 2023/09/27 23:51:10 by rrhnizar         ###   ########.fr       */
+/*   Updated: 2023/09/29 12:27:01 by rrhnizar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,20 +21,21 @@ int main(int argc, char **argv)
 			std::cout << "error in databse !! " << std::endl;
 			return 1;
 		}
-		std::ifstream file;
-		file.open(argv[1]);
-		if(!file.is_open())
-		{
-			std::cerr << "Error opening input file." << std::endl;
-			return 1;
-		}
-		std::string line;
-		while (std::getline(file, line))
-		{
-			if(findPipe(line) == 0)
-				std::cout << "Error: bad input => " << line << std::endl;
-		}
-		file.close();
+		(void)argv;
+		// std::ifstream file;
+		// file.open(argv[1]);
+		// if(!file.is_open())
+		// {
+		// 	std::cerr << "Error opening input file." << std::endl;
+		// 	return 1;
+		// }
+		// std::string line;
+		// while (std::getline(file, line))
+		// {
+		// 	if(findPipe(line) == 0)
+		// 		std::cout << "Error: bad input => " << line << std::endl;
+		// }
+		// file.close();
 	}
 	else
 		std::cout << "Error: could not open file." << std::endl;
