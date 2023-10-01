44469c67d85dcee4984be77068a9fdaba182b864
Rida Rhnizar <rrhnizar@c1r2p5.1337.ma>
2023-10-02 00:53:39 +0100

    i think finish this exercice but should change the container

--- ex01/RPN.hpp
+++ ex01/RPN.hpp
@@ -6,8 +6,9 @@
 /*   By: rrhnizar <rrhnizar@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/01 15:41:15 by rrhnizar          #+#    #+#             */
-/*   Updated: 2023/10/01 20:54:43 by rrhnizar         ###   ########.fr       */
+/*   Updated: 2023/10/01 21:46:47 by rrhnizar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <vector>
